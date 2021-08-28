461e9c8c8071951817612b84f8a97a7fc21ab202
Charles <charlesrichson@gmail.com>
2021-08-28 15:39:38 +0100

    Basic inital setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h2>Team Coelho</h2>
+        <p>Zuru Plugin Channels</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
