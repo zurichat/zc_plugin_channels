c241362428c74372a5f70db8173cb9642c17290a
AY-BAMZ <aybamz181@gmail.com>
2021-09-01 03:10:59 +0100

    thread structure updated

--- frontend/src/components/home/threads/Threads.js
+++ frontend/src/components/home/threads/Threads.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import ThreadMessage from './threadMessage/ThreadMessage.js'
-import ThreadHeader from './threadHeader/ThreadHeader.js'
+import ThreadMessage from './threadMessage/ThreadMessage'
+import ThreadHeader from './threadHeader/ThreadHeader'
 import ThreadReplies from './threadReply/ThreadReplies'
 import ThreadInputForm from './threadInputForm/ThreadInputForm'
 
