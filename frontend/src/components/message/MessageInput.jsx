6af9ae50ba92620f1acc41a2eefe19cd69aa0fd8
odelolajosh <oladapoodelola@gmail.com>
2021-09-01 10:05:33 +0100

    Designed the MessageInput component

--- frontend/src/components/message/MessageInput.jsx
+++ frontend/src/components/message/MessageInput.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Box } from '@chakra-ui/layout'
-import { Input } from '@chakra-ui/react'
+import { Flex, HStack, Input, Spacer, Stack, Textarea } from '@chakra-ui/react'
+import { BoldIcon, CopyLinkIcon, HSeparatorIcon, ItalicsIcon, ListIcon, MentionIcon, SendIcon, StrikeIcon } from '../icon'
 
 const MessageInput = () => {
   return (
@@ -11,13 +12,28 @@ const MessageInput = () => {
         bottom='22px'
         border='1px solid #EBEBEB'
         bg='white'
+        borderRadius='3px'
     > 
-        <Input height='54px' border='none' placeholder='Send a message to John' paddingBlock='18px' paddingInline='19px' _focus={{ border: 'none' }} />
-        <Box>
-            
-        </Box>
+        {/* <Input height='54px' border='none' fontSize='15px' placeholder='Send a message to John' _placeholder={{ color: 'neutral.500' }} paddingBlock='18px' paddingInline='20px' _focus={{ border: 'none' }} /> */}
+        <Textarea height='54px' border='none' fontSize='15px' placeholder='Send a message to John' _placeholder={{ color: 'neutral.500' }} paddingBlock='18px' paddingInline='20px' _focus={{ border: 'none' }} />
+        <Flex paddingTop='5px' paddingBottom='10px' paddingInline='20px'>
+            <HStack spacing='5px'>
+                <Box><StrikeIcon /></Box>
+                <HSeparatorIcon />
+                <Box><BoldIcon /></Box>
+                <Box><ItalicsIcon /></Box>
+                <Box><ListIcon /></Box>
+            </HStack>
+            <Spacer />
+            <Stack direction='row-reverse' spacing='5px'>
+                <Box><SendIcon /></Box>
+                <Box><CopyLinkIcon /></Box>
+                <Box><MentionIcon /></Box>    
+            </Stack>
+        </Flex>
     </Box>
   )
 }
+
 
 export default MessageInput
