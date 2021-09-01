3099c0bd45ae4517c39fafc812deb3e545d7bbd7
AY-BAMZ <aybamz181@gmail.com>
2021-09-01 02:45:44 +0100

    Thread components structure created

---
+++ frontend/src/components/home/threads/Threads.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ThreadMessage from './threadMessage/ThreadMessage'
+import ThreadHeader from './threadHeader/ThreadHeader'
+import ThreadReplies from './threadReply/ThreadReplies'
+import ThreadInputForm from './threadInputForm/ThreadInputForm'
+
+function Threads() {
+    return (
+        <div className='threadComponent'>
+            <ThreadHeader />
+            <ThreadMessage />
+            <ThreadReplies />
+            <ThreadInputForm />
+        </div>
+    )
+}
+
+export default Threads
---
+++ frontend/src/components/home/threads/threadReply/ThreadReplies.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ThreadReply from './ThreadReply'
+
+function ThreadReplies() {
+    return (
+        <div>
+            <ThreadReply />
+            <ThreadReply />
+            <ThreadReply />
+        </div>
+    )
+}
+
+export default ThreadReplies
