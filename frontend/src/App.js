11e4fa63c1a28c38f3cdbf3be3bc4e2e53b942ab
Harith Onigemo <onigemotosin@gmail.com>
2021-09-02 03:29:54 +0400

    MessageCard component completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,24 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import routes from './routes/routes'
 import store from './redux/store'
+import MessageCard from './components/MessageBoard/subs/MessageCard/MessageCard'
+import EachMessage from './components/admin/subs/MessageBoardTop/EachMessage'
+import MessageBoardTop from './components/admin/subs/MessageBoardTop/MessageBoardTop'
+import MessageCardContainer from './components/MessageBoard/subs/MessageCardContainer/MessageCardContainer'
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route component={routes} />
-        </Switch>
-      </Router>
-    </Provider>
+    // <Provider store={store}>
+    //   <Router>
+    //     <Switch>
+    //       <Route component={routes} />
+    //     </Switch>
+    //   </Router>
+    // </Provider>
+    // <MessageCard />
+    // <EachMessage />
+    // <MessageBoardTop />
+    <MessageCardContainer />
   )
 }
 
