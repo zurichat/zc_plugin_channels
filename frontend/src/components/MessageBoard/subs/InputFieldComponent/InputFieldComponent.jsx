2a1ab42f269bd0dbdcd4303aed30650dd82d86dd
sther <estheradeyemi796@gmail.com>
2021-09-02 15:44:21 +0100

    inputFieldComponent

---
+++ frontend/src/components/MessageBoard/subs/InputFieldComponent/InputFieldComponent.jsx
@@ -0,0 +1,42 @@
+import React,{useState} from 'react'
+import { ChakraProvider,extendTheme,Box,Text,Divider, Textarea,input, Input, Button } from "@chakra-ui/react"
+import { AtSignIcon } from '@chakra-ui/icons'
+import {IoFlashOutline, IoSendSharp} from 'react-icons/io5';
+import {BsTypeBold,BsLink45Deg} from 'react-icons/bs';
+import {FiItalic} from 'react-icons/fi';
+import {AiOutlineBars} from 'react-icons/ai';
+import {GrAttachment} from 'react-icons/gr'
+
+function App() {
+  const [input,setInput]=useState(false)
+  return (
+    <ChakraProvider>
+      <Box p={3}>
+      <Box display="flex" flexDirection="column" borderWidth="1px" p={3}>
+        <Input variant="unstyled" placeholder="Send a Message" size="lg" fontSize="md" onInput={()=>setInput(true)} onMouseOut={()=>setInput(false)}/>
+          <Box maxW="100%" display="flex" justifyContent="space-between" mt={5} flexDirection="row" alignItems="center">
+            <Box width={{base:"100px",sm:"150px",md:'180px'}} display="flex" flexDirection="row" justifyContent="space-between"
+              _hover={{ cursor: "pointer" }}>
+                <IoFlashOutline/>
+                <Divider height="20px" color="gray.500" orientation="vertical" />
+                <BsTypeBold/>
+                <FiItalic/>
+                <BsLink45Deg/>
+                <AiOutlineBars/>
+            </Box>
+            <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center" width={{base:"60px",sm:"80px",md:'120px'}}
+            _hover={{ cursor: "pointer" }}>
+              <AtSignIcon/>
+              <GrAttachment/>
+              {
+                input ? <Button bg="#00B87C" size="xs"><IoSendSharp color="white"/></Button> : <IoSendSharp/>
+              }
+            </Box>
+          </Box>
+      </Box>
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
