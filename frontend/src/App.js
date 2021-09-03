23e05ec0a0779b1bd4a7049c80dc4e59dd8b7799
aroyehunahmad <ahmadadewale800@gmail.com>
2021-09-03 19:44:40 +0100

    Thread replies created and deleted MessagesList was in the wrong place

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import routes from './routes/routes'
 import store from './redux/store'
 import Home from './components/home/Home'
+import Replies from './components/thread/subs/ThreadReplies'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Route component={routes} />
       </Router>
+      <Replies />
     </Provider>
   )
 }
