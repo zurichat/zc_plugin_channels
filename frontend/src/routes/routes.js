f165f7f224a62685cb7053e60c32891099e78060
Charles <charlesrichson@gmail.com>
2021-09-03 14:45:52 +0100

    updated routes.js

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -3,16 +3,10 @@ import Home from '../components/home/Home'
 import Admin from '../components/admin/index'
 import CreateChannel from '../components/createChannel/index'
 import MessageBoardIndex from '../components/MessageBoard/MessageBoardIndex'
-<<<<<<< HEAD
-=======
-// import TempSidebar from '../components/TempSidebar/index'
-import { Box, HStack } from '@chakra-ui/layout'
->>>>>>> 2576487887c997322b9f2015fd98313d1d01a1a8
 
 const routes = () => {
   return (
     <Switch>
-<<<<<<< HEAD
       <Route exact path='/'>
         <Home />
       </Route>
@@ -25,22 +19,6 @@ const routes = () => {
       <Route path='/message-board'>
         <MessageBoardIndex />
       </Route>
-=======
-      <HStack>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route path='/create-channel'>
-          <CreateChannel />
-        </Route>
-        <Route path='/admin'>
-          <Admin />
-        </Route>
-        <Route path='/message-board'>
-          <MessageBoardIndex />
-        </Route>
-      </HStack>
->>>>>>> 2576487887c997322b9f2015fd98313d1d01a1a8
     </Switch>
   )
 }
