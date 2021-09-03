ab054580015df9516e7e0584e64fe59040e1dc2c
Abraham Ogbumah <ab.ogbumah@gmail.com>
2021-09-03 18:02:32 +0100

    update to various files

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -3,22 +3,32 @@ import Home from '../components/home/Home'
 import Admin from '../components/admin/index'
 import CreateChannel from '../components/createChannel/index'
 import MessageBoardIndex from '../components/MessageBoard/MessageBoardIndex'
+import ChannelDetailsAndSetting from '../components/channelDetailsAndSetting/index'
+import TempSidebar from '../components/TempSidebar/index'
+import { Box, HStack } from '@chakra-ui/layout'
 
 const routes = () => {
   return (
     <Switch>
-      <Route exact path='/'>
-        <Home />
-      </Route>
-      <Route path='/create-channel'>
-        <CreateChannel />
-      </Route>
-      <Route path='/admin'>
-        <Admin />
-      </Route>
-      <Route path='/message-board'>
-        <MessageBoardIndex />
-      </Route>
+      <HStack>
+        <TempSidebar />
+
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/create-channel'>
+          <CreateChannel />
+        </Route>
+        <Route path='/admin'>
+          <Admin />
+        </Route>
+        <Route path='/message-board'>
+          <MessageBoardIndex />
+        </Route>
+        <Route path='/channel-detail'>
+          <ChannelDetailsAndSetting />
+        </Route>
+      </HStack>
     </Switch>
   )
 }
