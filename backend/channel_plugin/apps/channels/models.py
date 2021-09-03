5c6adbfc24c7daab04cd8fbf9dabd0ed4c791550
DarlsyLida <lidadarlsy@gmail.com>
2021-09-04 02:34:01 +0300

    descriptive commit message

--- backend/channel_plugin/apps/channels/models.py
+++ backend/channel_plugin/apps/channels/models.py
@@ -1,3 +1,9 @@
 # from django.db import models
 
 # Create your models here.
+from django.db import models
+
+
+class Thread(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    mmsg = models.CharField(max_length=50, blank=False, null=False)
