3c57b862653cc33784a72451f194a26a1512b70f
Favvie <favourprecious3@gmail.com>
2021-09-03 22:04:18 +0100

    added the user profile on hover

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Box } from "@chakra-ui/layout";
 import UserProfileHeader from "./UserProfileHeader";
+import UserProfileOnHover from './UserProfileOnHover';
 
 const userProfile = () => {
   return <Box>
               <UserProfileHeader />
+              <UserProfileOnHover />
         </Box>;
 };
 
