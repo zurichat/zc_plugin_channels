21995606d2ab94294d9b889beb2d8e4dd6f3ba83
th3nn3ss <chuksmcdennis@yahoo.com>
2021-09-04 06:59:08 +0100

    added version and description to info/views.py

--- backend/channel_plugin/channel_plugin/info/views.py
+++ backend/channel_plugin/channel_plugin/info/views.py
@@ -8,6 +8,7 @@ from rest_framework.decorators import api_view
 from datetime import datetime
 import random
 
+
 class GetInfoViewset(ViewSet):
     @action(
         methods=["GET"],
@@ -18,8 +19,7 @@ class GetInfoViewset(ViewSet):
             "name": settings.TEAM_NAME,
             "project": settings.PROJECT_NAME,
             "version": "1.0",
-            "description": "The Channel Plugin is a feature that helps users create spaces for conversation and communication on zuri.chat. Users can also create sub tags in the channels option where other things can be done, ranging from game nights, football banter, random, announcement and so much more. This adds the feature of having organized conversations in dedicated spaces called channels."
-
+            "description": "The Channel Plugin is a feature that helps users create spaces for conversation and communication on zuri.chat. Users can also create sub tags in the channels option where other things can be done, ranging from game nights, football banter, random, announcement and so much more. This adds the feature of having organized conversations in dedicated spaces called channels.",
         }
         return Response(data, status=status.HTTP_200_OK)
 
@@ -28,26 +28,26 @@ class GetInfoViewset(ViewSet):
         data = {
             "name": settings.TEAM_NAME,
             "project": settings.PROJECT_NAME,
-<<<<<<< HEAD
             "version": "1.0",
-            "description": "The Channel Plugin is a feature that helps users create spaces for conversation and communication on zuri.chat. Users can also create sub tags in the channels option where other things can be done, ranging from game nights, football banter, random, announcement and so much more. This adds the feature of having organized conversations in dedicated spaces called channels."
-=======
-            "description": "Lorem ipsum dolor sit amet, \
-                consectetur adipiscing elit.",
->>>>>>> dev
+            "description": "The Channel Plugin is a feature that helps users create spaces for conversation and communication on zuri.chat. Users can also create sub tags in the channels option where other things can be done, ranging from game nights, football banter, random, announcement and so much more. This adds the feature of having organized conversations in dedicated spaces called channels.",
         }
         return Response(data, status=status.HTTP_200_OK)
 
+
 now = datetime.now()
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def vistingDetail(request):
-    if request.method == 'GET':
+    if request.method == "GET":
 
         date = now.strftime("%m/%d/%Y, %H:%M:%S")
         no_of_times = random.randint(11, 25) + random.randint(10, 20)
-        return Response(data = {
-            "message": "Welcome, to the Channels Plugin",
-             "last_visted": date, 
-             "no_of_times_visted": no_of_times 
-        }, status=status.HTTP_200_OK)
+        return Response(
+            data={
+                "message": "Welcome, to the Channels Plugin",
+                "last_visted": date,
+                "no_of_times_visted": no_of_times,
+            },
+            status=status.HTTP_200_OK,
+        )
