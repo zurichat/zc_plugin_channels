61916d7d68d01b1d59a8b1274ccb97b7e3851ea1
Afolabi Asogba <apholy2010@gmail.com>
2021-09-04 11:43:00 +0100

    created an endpoint to send messages to a channel (#412)

--- backend/channel_plugin/apps/channels/urls.py
+++ backend/channel_plugin/apps/channels/urls.py
@@ -1,5 +1,6 @@
 from apps.channels import views
 from apps.channels.views import Test, SearchMessagesAPIView, GetChannelInfo, create_channel, GetChannelRoles
+from apps.channels.views import SendMessageInChannel
 from django.urls import path
 
 urlpatterns = [
@@ -8,5 +9,7 @@ urlpatterns = [
     path("<int:pk>/", GetChannelInfo.as_view()),
     path("create_channel/", create_channel, name="api_create_channel"),
     path("search_messages/", SearchMessagesAPIView.as_view(), name='api_search_messages'),
+    path("messages/", SendMessageInChannel.as_view()),
+
     path("<int:channel_id>/delete/", views.channel_delete, name='delete_channel')
 ]
