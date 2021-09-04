b2049436c8d68b00c96698a9774baf0326fe2b4e
AY-BAMZ <59142434+AY-BAMZ@users.noreply.github.com>
2021-09-04 16:58:35 +0100

    Thread MainMessage added

--- frontend/src/components/thread/Thread.jsx
+++ frontend/src/components/thread/Thread.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ThreadHeader from "./subs/ThreadHeader"
 import MessageForm from "./subs/MessageForm"
+import MainMessage from "./subs/MainMessage"
 import ThreadReplies from "./subs/ThreadReplies"
 import { Container } from "@chakra-ui/react"
 
@@ -8,6 +9,7 @@ function Thread() {
     return(
       <Container maxW="370px"> 
         <ThreadHeader />
+<MainMessage />
         <ThreadReplies />
         <MessageForm />
       </Container>
