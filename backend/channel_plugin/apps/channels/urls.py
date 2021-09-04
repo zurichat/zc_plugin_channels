baa1eb28c23748daff1c0938849fba3bc78648a7
Joseph Adebowale <adebowalejojo2001@gmail.com>
2021-09-04 04:09:38 +0200

    added thread creation endpoint

--- backend/channel_plugin/apps/channels/urls.py
+++ backend/channel_plugin/apps/channels/urls.py
@@ -1,9 +1,22 @@
-from apps.channels.views import Test, SearchMessagesAPIView, GetChannelInfo, create_channel
+from apps.channels.views import (
+    CreateChannelView,
+    GetChannelInfo,
+    SearchMessagesAPIView,
+    Test,
+    create_channel,
+)
 from django.urls import path
 
 urlpatterns = [
     path("test/", Test.as_view()),
     path("<int:pk>/", GetChannelInfo.as_view()),
     path("create_channel/", create_channel, name="api_create_channel"),
-    path("search_messages/", SearchMessagesAPIView.as_view(), name='api_search_messages'),
+    path(
+        "search_messages/", SearchMessagesAPIView.as_view(), name="api_search_messages"
+    ),
+    path(
+        "organizations/<organization_id>/channels/<channel_id>/threads/",
+        CreateChannelView.as_view(),
+        name="Create-Thread-Endpoint",
+    ),
 ]
