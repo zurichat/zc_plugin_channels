9b357823197e2fdcce6a65daf3a328d9b25ad771
bello hadi <bellohadi82@gmail.com>
2021-09-04 20:39:47 +0100

    adjusted conflict

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Box } from "@chakra-ui/layout";
 import UserProfileHeader from "./UserProfileHeader";
-import UserProfileOnHover from './UserProfileOnHover';
+// import UserProfileOnHover from './UserProfileOnHover';
+import OnClickUserProfile from "./OnClickUserProfile";
 
 const userProfile = () => {
   return <Box>
               <UserProfileHeader />
-              <UserProfileOnHover />
+              <OnClickUserProfile />
+              {/* <UserProfileOnHover /> */}
         </Box>;
 };
 
