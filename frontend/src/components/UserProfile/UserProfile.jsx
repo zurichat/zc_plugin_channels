43b6f231752282b2b634d70738fcec71449243c4
bello hadi <bellohadi82@gmail.com>
2021-09-04 18:39:20 +0100

    import onclcik user profile into userprofile

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Box } from "@chakra-ui/layout";
 import UserProfileHeader from "./UserProfileHeader";
+import OnClickUserProfile from "./OnclickuserProfile";
 
 const userProfile = () => {
   return <Box>
               <UserProfileHeader />
+              <OnClickUserProfile />
         </Box>;
 };
 
