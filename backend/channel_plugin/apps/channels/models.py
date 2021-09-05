bd0ca2f65ba3c187da597712db25ff8e80641fea
Charles <charlesboy49@gmail.com>
2021-09-05 10:32:09 +0100

    Create request cycle #344 (#454)
    * test-pre-commit
    
    * add request class

--- backend/channel_plugin/apps/channels/models.py
+++ backend/channel_plugin/apps/channels/models.py
@@ -1,3 +1,198 @@
-# from django.db import models
+from dataclasses import dataclass
 
-# Create your models here.
+from django.utils import timezone
+
+from channel_plugin.channel_plugin.utils.customrequest import Request
+
+
+@dataclass
+class Channel:
+    # name of channel
+    name: str
+    # list of user IDs in a channel
+    users: list = []
+    # list of role IDs in a channel
+    roles: list = []
+    # description of channel
+    description: str = ""
+    # private / public
+    private: bool = False
+    # when channel was created
+    created_on: str = timezone.now().isoformat()
+
+    def create(self, organization_id):
+        payload = {
+            "name": self.name.lower(),
+            "description": self.description,
+            "private": self.private,
+            "user": self.users,
+            "roles": self.roles,
+            "created_on": self.created_on,
+        }
+        response = Request.post(
+            organization_id, self.__class__.__name__.lower(), payload
+        )
+
+        return response
+
+    """"
+    organization_id: str
+    params: dict (query params to be appended to url to get data)
+    """
+
+    def read(self, organization_id, params):
+        pass
+
+    """"
+    organization_id: str
+    kwargs: either filter (dict) or object_id (str)
+    """
+
+    def update(self, organization_id, **kwargs):
+        pass
+
+    """"
+    organization_id: str
+    kwargs: either filter (dict) or object_id (str)
+    """
+
+    def delete(self, organization_id, **kwargs):
+        pass
+
+    def __str__(self):
+        return self.name
+
+
+@dataclass
+class ChannelMessage:
+    user_id: str
+    channel_id: str
+    content: str
+    # list of thread IDs
+    thread: list
+    # list of thread emojis
+    emojis: list
+    pinned: bool = False
+    edited: bool = False
+    timestamp: str = timezone.now().isoformat()
+
+    def create(self, organization_id):
+        pass
+
+    """"
+    organization_id: str
+    params: dict (query params to be appended to url to get data)
+    """
+
+    def read(self, organization_id, params):
+        pass
+
+    """"
+    organization_id: str
+    kwargs: either filter (dict) or object_id (str)
+    """
+
+    def update(self, organization_id, **kwargs):
+        pass
+
+    """"
+    organization_id: str
+    kwargs: either filter (dict) or object_id (str)
+    """
+
+    def delete(self, organization_id, **kwargs):
+        pass
+
+    def __str__(self):
+        return self.content
+
+
+@dataclass
+class Thread:
+    user_id: str
+    channelmessage_id: str
+    content: str
+    emojis: list
+    edited: bool = False
+    timestamp: str = timezone.now().isoformat()
+
+    def create(self, organization_id):
+        pass
+
+    """"
+    organization_id: str
+    params: dict (query params to be appended to url to get data)
+    """
+
+    def read(self, organization_id, params):
+        pass
+
+    """"
+    organization_id: str
+    kwargs: either filter (dict) or object_id (str)
+    """
+
+    def update(self, organization_id, **kwargs):
+        pass
+
+    """"
+    organization_id: str
+    kwargs: either filter (dict) or object_id (str)
+    """
+
+    def delete(self, organization_id, **kwargs):
+        pass
+
+    def __str__(self):
+        return self.content
+
+
+@dataclass
+class Role:
+    """
+    Set of roles for a particular channel
+    """
+
+    name: str
+    channel_id: str
+    permission: list
+
+    def create(self, organization_id):
+        pass
+
+    """"
+    organization_id: str
+    params: dict (query params to be appended to url to get data)
+    """
+
+    def read(self, organization_id, params):
+        pass
+
+    """"
+    organization_id: str
+    kwargs: either filter (dict) or object_id (str)
+    """
+
+    def update(self, organization_id, **kwargs):
+        pass
+
+    """"
+    organization_id: str
+    kwargs: either filter (dict) or object_id (str)
+    """
+
+    def delete(self, organization_id, **kwargs):
+        pass
+
+    def __str__(self):
+        return self.name
+
+
+@dataclass
+class Permission:
+    name: str
+    key: str
+    description: str
+
+    def __str__(self):
+        return self.name
---
+++ backend/channel_plugin/channel_plugin/utils/customrequest.py
@@ -0,0 +1,80 @@
+import json
+from dataclasses import dataclass
+from os import write
+from urllib.parse import quote_plus
+
+import requests
+from django.conf import settings
+from django.http import JsonResponse
+
+data = {"plugin_id": settings.PLUGIN_ID, "bulk_write": False, "payload": {}}
+read = settings.READ_URL
+write = settings.WRITE_URL
+
+
+def check_payload(payload):
+    if type(payload) == list:
+        return True
+    assert type(payload) == dict, "payload must be list or dict"
+    return False
+
+
+@dataclass
+class Request:
+    @staticmethod
+    def get(org_id, collection, params=None):
+        url = f"{write}/{settings.PLUGIN_ID}/{collection}/{org_id}/?{params}"
+        if params is not None:
+            params = quote_plus(params)
+            url += f"?{params}"
+        response = requests.get(url)
+        if response.status_code >= 200 and response.status_code < 300:
+            return response.json()
+        return response.json()
+
+    @staticmethod
+    def post(org_id, collection, payload):
+        data.update(
+            {
+                "organization_id": org_id,
+                "collection_name": collection,
+                "bulk_write": check_payload(payload),
+                "payload": payload,
+            }
+        )
+        response = requests.post(write, data=json.dumps(data))
+        if response.status_code >= 200 and response.status_code < 300:
+            return response.json()
+        return response.json()
+
+    @staticmethod
+    def put(org_id, collection, payload, filter=None, object_id=None):
+        data.update(
+            {
+                "organization_id": org_id,
+                "collection_name": collection,
+                "bulk_write": check_payload(payload),
+                "payload": payload,
+            }
+        )
+        if data.get("bulk_write"):
+            if filter is None:
+                return JsonResponse(
+                    {"error": "Filter must be set for multiple payload"}
+                )
+            data.update({"filter": filter})
+        else:
+            if object_id is None:
+                return JsonResponse(
+                    {"error": "Object ID must be set for multiple payload"}
+                )
+            data.update({"object_id": object_id})
+
+        response = requests.post(write, data=json.dumps(data))
+        if response.status_code >= 200 and response.status_code < 300:
+            return response.json()
+        return response.json()
+
+    @staticmethod
+    def delete(org_id, collection, payload, filter=None, object_id=None):
+        raise NotImplementedError("Zuri Core has not implemeted")
