8652b2ee3fa1cd78d8c1a8e8a970905d51d1e3b5
Favvie <favourprecious3@gmail.com>
2021-09-05 10:46:27 +0100

    Final final commit

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Box } from "@chakra-ui/layout";
 import UserProfileHeader from "./UserProfileHeader";
 import UserProfileOnHover from './UserProfileOnHover';
-// import OnclickuserProfile from "./OnclickuserProfile";
+import OnclickuserProfile from "./OnclickuserProfile";
 
 const UserProfile = () => {
   return <Box>
               <UserProfileHeader />
               <UserProfileOnHover />
-              {/* <OnclickuserProfile /> */}
+              <OnclickuserProfile />
         </Box>;
 };
 
