47ce9c78d42877dbf1aa3ae11705cd5f127d9ee0
Joseph Adebowale <62403531+Joseph455@users.noreply.github.com>
2021-09-13 17:10:18 +0100

    Removed potential circular import

--- backend/channel_plugin/apps/centri/apps.py
+++ backend/channel_plugin/apps/centri/apps.py
@@ -3,7 +3,4 @@ from django.apps import AppConfig
 
 class CentriConfig(AppConfig):
     name = 'apps.centri'
-
-    def ready(self) -> None:
-        from apps.centri import signals
-        return super().ready()
+    
