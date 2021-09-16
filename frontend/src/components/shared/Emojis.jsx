f416461511b3688a6a87050014d40becbfb0035a
AY-BAMZ <aybamz181@gmail.com>
2021-09-16 14:16:21 +0100

    changes

--- frontend/src/components/shared/Emojis.jsx
+++ frontend/src/components/shared/Emojis.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Picker from 'emoji-picker-react';
+import { Container, Box } from "@chakra-ui/react"
 
 function Emojis() {
 
@@ -10,14 +11,15 @@ function Emojis() {
   };
 
   return (
-    <div style={{position: 'absolute', marginTop: -300}}>
+    <Container style={{position: 'absolute', marginTop: -300}}>
       {chosenEmoji ? (
-        <span>{chosenEmoji.emoji}</span>
+        <Box>The Emoji choosed is {chosenEmoji.emoji}</Box> // this is where the value of the emoji called will display, and you can get the value through this.
       ) : (
-        <span>Pick an Emoji</span>
+        <Box>Pick an Emoji</Box>
       )}
-      <Picker onEmojiClick={onEmojiClick} />
-    </div>
+      {/* this is the emoji component itself, and you will be able to click the component from here and get the value of the emoji clickes with the onEmojiClick hooks. */}
+      <Picker onEmojiClick={onEmojiClick} /> 
+    </Container>
   );
 };
 
