239b4ea55d4b8aa2ec1ddbb6a0036cd7c0daad4a
Joseph Adebowale <62403531+Joseph455@users.noreply.github.com>
2021-09-17 21:27:37 +0100

    implemented custom corsmiddleware (#533)
    * Implemented RTC for channel join/leave events
    
    * Removed potential circular import
    
    * corrected centrifugo_url  settings
    
    * commit
    
    * pre-merge commit
    
    * create filterwrapper class
    
    * pre-merge commit
    
    * fixed centrifugo api connection
    
    * fixed centrifugo api connection
    
    * implemented RTC for channelmessage endpoints
    
    * socket break test
    
    * Created socket test link
    
    * potential fix for channel join/leave RTC
    
    * fixs members endpoint RTC
    
    * fixed channel list members view
    
    * commit
    
    * refactored channels view
    
    * allowed all local host cors
    
    * Update production.py
    
    * implemented custom corsmiddleware
    
    * Update base.py
    
    Co-authored-by: Charles <charlesboy49@gmail.com>

--- backend/channel_plugin/channel_plugin/utils/middleware.py
+++ backend/channel_plugin/channel_plugin/utils/middleware.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.http import JsonResponse
+from django import http
 
 class AuthenticationMiddleware:
 
@@ -18,3 +20,19 @@ class AuthenticationMiddleware:
         # Process user token from zc_core & raise exception if
         # the user isnt authenticated
         # return JsonResponse({"msg": "user not authenticated"})
+
+
+class CorsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if (request.method == "OPTIONS"  and "HTTP_ACCESS_CONTROL_REQUEST_METHOD" in request.META):
+            response = http.HttpResponse()
+            response["Content-Length"] = "0"
+            response["Access-Control-Max-Age"] = 86400
+        response["Access-Control-Allow-Origin"] = request.headers.get("Origin") or "*"
+        response["Access-Control-Allow-Methods"] = "DELETE, GET, OPTIONS, PATCH, POST, PUT"
+        response["Access-Control-Allow-Headers"] = "accept, accept-encoding, authorization, content-type, dnt, origin, user-agent, x-csrftoken, x-requested-with"
+        return response
