133ea279ed4bf3dd46477977e94c1e2c8e874932
odelolajosh <oladapoodelola@gmail.com>
2021-09-18 22:27:25 +0100

    Fixed conflict

--- frontend/src/components/UserProfile/UserProfile.jsx
+++ frontend/src/components/UserProfile/UserProfile.jsx
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import { Box } from "@chakra-ui/layout";
-<<<<<<< HEAD
-import UserProfileOnHover from './UserProfileOnHover';
-import OnClickUserProfile from "./OnclickuserProfile";
-=======
 import UserProfileOnHover from "./UserProfileOnHover";
 import OnClickUserProfile from "./OnClickUserProfile";
->>>>>>> 790a3a1d74e343fce58c5dfaae703d6f16a7c1eb
 
 const UserProfile = () => {
   const [showProfile, setShowProfile] = useState(false);
