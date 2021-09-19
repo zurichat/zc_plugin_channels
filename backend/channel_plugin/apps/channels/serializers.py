61690a06ec51188b5b05230106f0e63c42e7debc
Charles <charlesboy49@gmail.com>
2021-09-19 15:57:34 +0100

    Revert "feat: user notifications endpoints. be.wrecodde (#544)"
    This reverts commit 13c3ec663d9348c8d8f3111190425925a21bf687.

--- backend/channel_plugin/apps/channels/serializers.py
+++ backend/channel_plugin/apps/channels/serializers.py
@@ -38,7 +38,6 @@ class UserSerializer(serializers.Serializer):
     _id = serializers.CharField(max_length=30, required=True)
     role_id = serializers.CharField(max_length=30, required=False)
     is_admin = serializers.BooleanField(default=False)
-    notifications = serializers.DictField(required=False)
 
 
 class ChannelGetSerializer(serializers.Serializer):
@@ -112,16 +111,8 @@ class UserChannelGetSerializer(serializers.Serializer):
     _id = serializers.ReadOnlyField()
     name = serializers.CharField(max_length=100, required=False)
     description = serializers.CharField(required=False)
-    notifications = serializers.DictField(required=False)
 
 
 class SocketSerializer(serializers.Serializer):
     socket_name = serializers.CharField(max_length=200, required=True) 
     channel_id = serializers.CharField(max_length=30, required=True)
-
-class NotificationsSettingSerializer(serializers.Serializer):
-
-    web = serializers.ChoiceField(choices=("all", "mentions", "nothing"))
-    mobile = serializers.ChoiceField(choices=("all", "mentions", "nothing"))
-    same_for_mobile = serializers.BooleanField(required=True)
-    mute = serializers.BooleanField(required=True)
