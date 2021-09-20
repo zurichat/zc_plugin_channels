b1f8691768cd1d2db5df9f1c9460a2e890973867
Matthew <80982101+Matthew2020-hub@users.noreply.github.com>
2021-09-20 17:17:44 +0800

    Manage message reactions (#553)
    * add: retrieve message reactions endpoint
    
    * add: update message reactions endpoint
    
    Co-authored-by: Deji <wrecodde@gmail.com>

--- backend/channel_plugin/apps/channelmessages/urls.py
+++ backend/channel_plugin/apps/channelmessages/urls.py
@@ -1,7 +1,8 @@
-from apps.channelmessages.views import channelmessage_views, channelmessage_views_group
+from apps.channelmessages.views import channelmessage_views, channelmessage_views_group, channelmessage_reactions
 from django.urls import path
 
 urlpatterns = [
     path("<str:org_id>/channels/<str:channel_id>/messages/", channelmessage_views),
     path("<str:org_id>/messages/<str:msg_id>/", channelmessage_views_group),
+    path("<str:org_id>/messages/<str:msg_id>/reactions/", channelmessage_reactions)
 ]
