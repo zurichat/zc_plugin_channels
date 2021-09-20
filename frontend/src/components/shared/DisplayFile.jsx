31092ff8d5cce8363021374e2b1b463db1cead8b
Osomhe1 <osomhe.aleogho@gmail.com>
2021-09-20 15:50:27 +0100

    update

--- frontend/src/components/shared/DisplayFile.jsx
+++ frontend/src/components/shared/DisplayFile.jsx
@@ -13,6 +13,7 @@ export default function DisplayFile() {
 
 
     // const [click, setClick] = useState(false);
+    //sometthing need to be changed
 
   const  handleGrid  = (e)=>{
       e.preventDefault();
