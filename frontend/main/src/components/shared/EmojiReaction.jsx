18f7ff5f0d2ca7012717ccf2897dfa1dad970441
AY-BAMZ <aybamz181@gmail.com>
2021-09-25 12:21:08 +0100

    leave channel and emojireaction changes

---
+++ frontend/main/src/components/shared/EmojiReaction.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useRef } from "react";
+import Picker from "emoji-picker-react";
+import { Container, Box } from "@chakra-ui/react"
+
+
+export default function App() {
+  const [message, setMessageForm] = useState("");
+  const ref = useRef(null);
+  const onEmojiClick = (event, emojiObject) => {
+    const cursor = ref.current.selectionStart;
+    const text = emojiObject.emoji;
+    setMessageForm(text);
+  };
+  const [count, setCount] = useState(null);
+
+  return (
+    <div>
+      
+      <Picker onEmojiClick={onEmojiClick} />
+      <Box cursor="pointer" alignItems="center" onClick={() => setCount(count + 1)} display="flex">
+      <Container
+        ref={ref}
+        onChange={e => setMessageForm(e.target.value)}
+      >{message}</Container><h4>{count + 1}</h4>
+      </Box>
+    </div>
+  );
+}
