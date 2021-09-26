de580e67848479ab0a79604b56e3bef59ed6873b
Harith Onigemo <onigemotosin@gmail.com>
2021-09-26 13:47:20 +0400

    Testing switch statements for centrifugo

--- frontend/main/src/components/MessageBoard/subs/Centrifugo/CentrifugoComponent.jsx
+++ frontend/main/src/components/MessageBoard/subs/Centrifugo/CentrifugoComponent.jsx
@@ -56,12 +56,14 @@ const CentrifugoComponent = () => {
 
   centrifuge.subscribe(sockets.socket_name, function(messageCtx) {
     console.log("from centrifugo: ", messageCtx);
+    // dispatch({ type: GET_RENDEREDMESSAGES, payload: renderedMessages.push(messageCtx.data) })
 
     let eventType = messageCtx.data.event.action
     let eventNumber = messageCtx.data.event.recipients
     switch (eventType) {
         case "join:channel":
           dispatch({ type: GET_RENDEREDMESSAGES, payload: renderedMessages.push(messageCtx.data) })
+          console.log("Testing switch statement: ", renderedMessages);
             break;
     
         default:
