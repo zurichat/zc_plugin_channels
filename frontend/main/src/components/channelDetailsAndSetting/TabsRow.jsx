624eff8b368261cec5fd8dc6dbb225f0c0f35450
Alegbe Mojisola <alegbeyemi@gmail.com>
2021-09-25 17:22:02 -0700

    channel details modal

--- frontend/main/src/components/channelDetailsAndSetting/TabsRow.jsx
+++ frontend/main/src/components/channelDetailsAndSetting/TabsRow.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { TabList, Tab } from "@chakra-ui/react"
 
-export default function TabsRows({colors}) {
- 
+export default function TabsRows({ colors }) {
+
     return (
         <>
-            <TabList borderBottom='0' justifyContent="space-between" mt={4} pt={4}>
-                <Tab p={0}  _focus={{outline: "none"}}>About</Tab>
-                <Tab p={0}  _focus={{outline: "none"}}>Members</Tab>
-                <Tab p={0}  _focus={{outline: "none"}}>Integration</Tab>
-                <Tab p={0}  _focus={{outline: "none"}}>Settings</Tab>
+            <TabList borderBottom='0' display='flex' mt={4} pt={4}>
+                <Tab p={2} _focus={{ outline: "none" }}>About</Tab>
+                <Tab p={2} _focus={{ outline: "none" }}>Members</Tab>
+                <Tab p={2} _focus={{ outline: "none" }}>Integration</Tab>
+                <Tab p={2} _focus={{ outline: "none" }}>Settings</Tab>
             </TabList>
         </>
     )
