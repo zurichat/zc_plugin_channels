9f5ae991a028360f2250d5ca5bf29fa377ebc715
Matthew2020-hub <akinolatolulope24@gmail.com>
2021-09-27 02:43:40 +0800

    thread reaction endpoint update

--- backend/channel_plugin/apps/threads/views.py
+++ backend/channel_plugin/apps/threads/views.py
@@ -9,7 +9,7 @@ from rest_framework.viewsets import ViewSet
 from channel_plugin.utils.customrequest import Request
 
 from .permissions import CanReply, IsMember, IsOwner
-from .serializers import ThreadSerializer, ThreadUpdateSerializer
+from .serializers import ThreadSerializer, ThreadUpdateSerializer, ThreadEmojiUpdateSerializer
 
 
 class ThreadViewset(ViewSet):
@@ -57,14 +57,14 @@ class ThreadViewset(ViewSet):
     )
     def thread_message(self, request, org_id, channelmessage_id):
         """Add reply to message
-        
+
         ```bash
         curl -X POST "{{baseUrl}}/v1/{{org_id}}/messages/{{channelmessage_id}}/threads/?channel_id={{channel_id}}"
-        -H  "accept: application/json" 
+        -H  "accept: application/json"
         -H  "Content-Type: application/json"
         -d "{
                 \"user_id\": \"string\",
-                \"content\": \"string\", 
+                \"content\": \"string\",
                 \"files\": [
                     \"string\"
                 ]
@@ -110,7 +110,7 @@ class ThreadViewset(ViewSet):
     )
     def thread_message_all(self, request, org_id, channelmessage_id):
         """Retrieve all replies to message
-        
+
         ```bash
         curl -X GET "{{baseUrl}}/v1/{{org_id}}/messages/{{channelmessage_id}}/threads/" -H  "accept: application/json"
         ```
@@ -154,7 +154,7 @@ class ThreadViewset(ViewSet):
     )
     def thread_message_update(self, request, org_id, thread_id):
         """Update thread message
-        
+
         ```bash
         curl -X PUT "{{baseUrl}}/v1/{{org_id}}/threads/{{thread_id}}/?user_id={{user_id}}&channel_id={{channel_id}}"
         -H  "accept: application/json"
@@ -201,7 +201,7 @@ class ThreadViewset(ViewSet):
     )
     def thread_message_delete(self, request, org_id, thread_id):
         """Delete thread message
-        
+
         ```bash
         curl -X DELETE "{{baseUrl}}/v1/{{org_id}}/threads/{{thread_id}}/?user_id={{user_id}}&channel_id={{channel_id}}" -H  "accept: application/json"
         ```
@@ -221,6 +221,103 @@ class ThreadViewset(ViewSet):
         )
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+    @action(
+        methods=["GET"],
+        detail=False,
+    )
+
+    def retrieve_thread_reactions(self, request, org_id, thread_id):
+        data = {"_id": thread_id}
+        data.update(dict(request.query_params))
+        result = Request.get(org_id, "thread", data) or {}
+        reactions = result.get("emojis", [])
+        status_code = status.HTTP_404_NOT_FOUND
+        if result.__contains__("_id") or isinstance(result, dict):
+            status_code = status.HTTP_200_OK
+        return Response(reactions, status=status_code)
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "user_id",
+                openapi.IN_QUERY,
+                description="User ID (owner of message)",
+                required=True,
+                type=openapi.TYPE_STRING,
+
+            ),
+            openapi.Parameter(
+                "channel_id",
+                openapi.IN_QUERY,
+                description="Channel ID (ID of channel message was posted)",
+                required=True,
+                type=openapi.TYPE_STRING,
+            ),
+        ],
+        request_body = ThreadEmojiUpdateSerializer,
+         responses={
+            200: openapi.Response("Response", ThreadEmojiUpdateSerializer),
+            404: openapi.Response("Not Found"),
+        },
+        operation_id="update-thread-reactions",
+    )
+
+    @action(
+        methods=["PUT"],
+        detail=False,
+    )
+    def update_thread_reactions(self, request, org_id, thread_id):
+
+        # get referenced message
+        data = {"_id": thread_id}
+        data.update(dict(request.query_params))
+        message = Request.get(org_id, "thread", data)
+
+        if message:
+            message_reactions = message.get("emojis", [])
+
+            serializer = ThreadEmojiUpdateSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+
+            # todo: refactor code to use dict instead of list
+            new_message_reaction = serializer.data
+            message_reaction = {
+                "title": new_message_reaction["title"],
+                "count": 1,
+                "users": [new_message_reaction["member_id"]]
+            }
+            message_reaction_index = None
+            for reaction in message_reactions:
+                if reaction['title'] == new_message_reaction['title']:
+                    message_reaction_index = message_reactions.index(reaction)
+                    message_reaction = reaction
+                    if new_message_reaction["member_id"] in message_reaction["users"]:
+                        message_reaction["count"] -= 1
+                        message_reaction["users"].remove(new_message_reaction["member_id"])
+                    else:
+                        message_reaction["count"] += 1
+                        message_reaction["users"].append(new_message_reaction["member_id"])
+
+            if message_reaction_index:
+                message_reactions.pop(message_reaction_index)
+                message_reactions.insert(message_reaction_index, message_reaction)
+            else:
+                message_reactions.append(message_reaction)
+
+            payload = {
+                "emojis": message_reactions
+            }
+            result = Request.put(org_id, "thread", payload, object_id=thread_id)
+            status_code = status.HTTP_400_BAD_REQUEST
+            if result:
+                return Response(message_reactions, status=status.HTTP_200_OK)
+
+            return Response({"error": "failed to update reaction"}, status=status_code)
+
+        status_code = status.HTTP_404_NOT_FOUND
+        return Response({"error": "message not found"}, status=status_code)
+
+
 
 thread_views = ThreadViewset.as_view(
     {
@@ -230,4 +327,7 @@ thread_views = ThreadViewset.as_view(
 )
 thread_views_group = ThreadViewset.as_view(
     {"put": "thread_message_update", "delete": "thread_message_delete"}
+)
+thread_reactions = ThreadViewset.as_view(
+    {"get": "retrieve_thread_reactions", "put": "update_thread_reactions"}
 )
