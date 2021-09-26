fc124f97524223880d5820c19c800872cfe62abf
Harith Onigemo <onigemotosin@gmail.com>
2021-09-26 14:31:43 +0400

    Testing switch statements for centrifugo

--- frontend/main/src/components/MessageBoard/subs/Centrifugo/CentrifugoComponent.jsx
+++ frontend/main/src/components/MessageBoard/subs/Centrifugo/CentrifugoComponent.jsx
@@ -56,7 +56,7 @@ const CentrifugoComponent = () => {
 
   centrifuge.subscribe(sockets.socket_name, function(messageCtx) {
     console.log("from centrifugo: ", messageCtx);
-    dispatch({ type: GET_RENDEREDMESSAGES, payload: renderedMessages.push(messageCtx.data) })
+    dispatch({ type: GET_RENDEREDMESSAGES, payload: renderedMessages.concat([messageCtx.data]) })
     console.log("Testing rendered messages: ", renderedMessages);
 
     let eventType = messageCtx.data.event.action
