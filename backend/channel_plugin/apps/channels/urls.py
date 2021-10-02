7f8b6ff09705ca05b9dd68244e7fb828cbc996d3
jamido1 <66392050+jamido1@users.noreply.github.com>
2021-10-02 23:54:00 +0100

    created RTC signals for notification changes, did the remove user from all channels endpoint

--- backend/channel_plugin/apps/channels/urls.py
+++ backend/channel_plugin/apps/channels/urls.py
@@ -6,7 +6,7 @@ from apps.channels.views import (
     channel_members_update_retrieve_views,
     channel_retrieve_update_delete_view,
     channel_socket_view,
-    user_channel_list,
+    user_channel_list_delete,
     notification_views,
 )
 from django.urls import path
@@ -36,6 +36,6 @@ urlpatterns = [
         "<str:org_id>/channels/<str:channel_id>/members/<str:member_id>/",
         channel_members_update_retrieve_views,
     ),
-    path("<str:org_id>/channels/users/<str:user_id>/", user_channel_list),
+    path("<str:org_id>/channels/users/<str:user_id>/", user_channel_list_delete),
     
 ]
