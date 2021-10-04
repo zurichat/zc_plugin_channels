4ad03555f1f228f9086a6d20551a449373d26257
odelolajosh <oladapoodelola@gmail.com>
2021-10-04 09:01:54 +0100

    Added centrifugo adapter

---
+++ frontend/main/src/utils/centrifugo.js
@@ -0,0 +1,39 @@
+import { SubscribeToChannel, GetUserInfo } from "@zuri/control";
+
+class Centrifugo {
+
+    constructor() {
+        this.listeners = {}
+        this.org_id = null
+        this.init()
+    }
+
+    async init() {
+        const { org_id } = (await GetUserInfo())["0"]
+        this.org_id = org_id
+        this.subscribeToAllEvents();
+    }
+
+    subscribeToAllEvents() {
+        SubscribeToChannel(`organizations_${this.org_id}`, (ctx) => {
+            console.log("centrifugo ctx == ", ctx)
+            this.listenToEvents(ctx)
+        })
+    }
+
+    addListener(event, callback) {
+        console.log("add listener == ", event)
+        this.listeners[event] = callback
+    }
+
+    listenToEvents(ctx) {
+        const { event, id, type } = ctx.data
+        console.log("centrifugo ctx ==", ctx + "for event == " + event)
+        if (this.listeners[event]) {
+            this.listeners[event](id, type, this.org_id)
+        }
+    }
+}
+
+
+export default new Centrifugo()
