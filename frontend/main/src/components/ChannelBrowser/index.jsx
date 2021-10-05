bc1159c1e5b5e2f7ea5e67390d67e255b09df4e9
kelani aliyu <kelanialiyu@gmail.com>
2021-10-05 02:56:26 +0100

    added sort and search fxnalities

--- frontend/main/src/components/ChannelBrowser/index.jsx
+++ frontend/main/src/components/ChannelBrowser/index.jsx
@@ -32,6 +32,186 @@ const ChannelBrowser = () => {
     }
   }, [orgId]);
 
+  let originalchannel = useSelector((state) => state.appReducer).channels;
+  const originalchannels = [
+    {
+      name: "Channel One",
+      _id: "001",
+      members: 12,
+      created_on: 123,
+      description: "test channel 1 yes",
+      users: {
+        name: "Jane doe"
+      }
+    },
+    {
+      name: "Channel Two",
+      _id: "002",
+      members: 27,
+      created_on: 124,
+      description: "test channel 2 haa",
+      users: {
+        name: "Jane doe"
+      }
+    },
+    {
+      name: "Channel 3",
+      _id: "003",
+      members: 12,
+      created_on: 125,
+      description: "test channel Three hmm",
+      users: {
+        name: "Jane doe"
+      }
+    },
+    {
+      name: "Channel Four",
+      _id: "004",
+      members: 12,
+      created_on: 126,
+      description: "test channel 4 suh",
+      users: {
+        name: "Jane doe"
+      }
+    },
+    {
+      name: "Channel Five",
+      _id: "005",
+      members: 5,
+      created_on: 127,
+      description: "test channel 5 umm",
+      users: {
+        name: "Jane doe"
+      }
+    },
+    {
+      name: "Channel 6",
+      _id: "006",
+      members: 12,
+      created_on: 128,
+      description: "test channel 6 yey",
+      users: {
+        name: "Jane doe"
+      }
+    },
+    {
+      name: "Channel Seven",
+      _id: "007",
+      members: 4,
+      created_on: 129,
+      description: "test channel 7 faa",
+      users: {
+        name: "Jane doe"
+      }
+    },
+    {
+      name: "Channel Eight",
+      _id: "008",
+      members: 7,
+      created_on: 1210,
+      description: "test channel 8 huh",
+      users: {
+        name: "Jane doe"
+      }
+    },
+    {
+      name: "Channel Nine",
+      _id: "009",
+      members: 1,
+      created_on: 1211,
+      description: "test channel 9 wee",
+      users: {
+        name: "Jane doe"
+      }
+    },
+    {
+      name: "Channel Ten",
+      _id: "010",
+      members: 10,
+      created_on: 1212,
+      description: "test channel 10 nay",
+      users: {
+        name: "Jane doe"
+      }
+    },
+  ] 
+
+  const [channels, setChannel] = useState([...originalchannel]);
+
+  useEffect(() => {
+    setChannel([...originalchannel])
+  }, [originalchannel.length])
+  
+  const searchChannel= param =>{
+    if (!channels) return
+    param = param.trim()
+    if(param){
+      setChannel(originalchannel.filter((chan) => (chan.name.includes(param) || chan.description.includes(param))))
+    }
+    else{
+      setChannel([...originalchannel])
+    }
+    
+  }
+
+  const sortBy = param =>{
+    if (!channels) return
+    const newChannel = channels.sort((chan1, chan2)=>{
+      let prop1=0
+      let prop2 =0
+
+      switch (param) {
+        case "recommended":
+          prop1 = chan1._id
+          prop2 = chan2._id
+          break;
+      
+        case "newest":
+          prop1 = new Date(chan1.created_on).getTime()
+          prop2 = new Date(chan2.created_on).getTime()
+          break;
+        
+        case "oldest":
+          prop1 = new Date(chan2.created_on).getTime()
+          prop2 = new Date(chan1.created_on).getTime()
+          break;
+        
+        case "leastMembers":
+          prop1 = chan1.members
+          prop2 = chan2.members
+          break;
+        
+        case "mostMembers":
+          prop1 = chan2.members
+          prop2 = chan1.members
+          break;
+        
+        case "name":
+          prop1 = chan1.name
+          prop2 = chan2.name
+          break;
+        
+        case "nameReverse":
+          prop1 = chan2.name
+          prop2 = chan1.name
+          break;
+        
+        default:
+          prop1 = chan1._id 
+          prop2 = chan2._id 
+          break;
+      }
+      if(typeof prop1 == "number"){
+        return prop1 - prop2
+      }
+      else{
+        return prop1 > prop2?1:prop1==prop2?0:-1 
+      }
+      
+      
+    })
+    setChannel([...newChannel])
+  }
   return (
     <Box mt="0" bgColor="#E5E5E5" height="100vh">
       {/* <ChannelBrowserHeader /> */}
@@ -44,8 +224,8 @@ const ChannelBrowser = () => {
         pt="16px"
         sx={{ "@media screen and (max-width: 768.5px)": { marginRight: "0" } }}
       >
-        <SearchMenu />
-        <ChannelList orgId={orgId} />
+        <SearchMenu channels={channels} sortBy={sortBy} searchChannel={searchChannel}/>
+        <ChannelList orgId={orgId} channels={channels}/>
       </Box>
 
       {/* Mobile View to Add Channel */}
