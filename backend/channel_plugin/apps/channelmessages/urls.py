50fbd8e2ad3141fa196c4cf97506088a5766d568
ajibadeabd <43596899+ajibadeabd@users.noreply.github.com>
2021-10-12 12:08:12 +0100

    Test branch search (#843)
    * pulled latest
    
    * created test file
    
    * fixed a bug from my end

--- backend/channel_plugin/apps/channelmessages/urls.py
+++ backend/channel_plugin/apps/channelmessages/urls.py
@@ -1,7 +1,6 @@
 # from apps.channelmessages.views import channelmessage_views, channelmessage_views_group, channelmessage_reactions, search_channelmessage
 from apps.channelmessages import views
 from django.urls import path
-
 urlpatterns = [
     path("<str:org_id>/channels/<str:channel_id>/messages/", views.channelmessage_views),
     path("<str:org_id>/messages/<str:msg_id>/", views.channelmessage_views_group),
@@ -9,5 +8,8 @@ urlpatterns = [
     path("<str:org_id>/channels/<str:channel_id>/search_messages/", views.search_channelmessage),
     path("<str:org_id>/channels/<str:channel_id>/messages/paginated/", views.paginate_messages,name = "paginate_messages"),
     path("<str:org_id>/channels/<str:channel_id>/messages/cursor/", views.get_user_cursor),
+    path("<str:org_id>/search", views.search),
+    path("test", views.test)
+    # path("test", views.search_channelmessage)
  
 ]
