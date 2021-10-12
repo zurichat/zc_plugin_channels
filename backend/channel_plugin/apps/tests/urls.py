65176b29313224dbf9f16e35feb65c35bf5e7510
TobeTek <82294671+TobeTek@users.noreply.github.com>
2021-10-12 14:14:24 +0100

    Created Test Page For Major Endpoints (#848)
    * Created Test for Major Pages Channels
    
    * fixed timeout error- test page
    
    * fix bugs on main test page

--- backend/channel_plugin/apps/tests/urls.py
+++ backend/channel_plugin/apps/tests/urls.py
@@ -1,7 +1,9 @@
-from django.urls import re_path
+from django.urls import re_path, path
 
 from . import views
 
 urlpatterns = [
-    re_path(r"^test/$", views.test_channel_endpoints, name="test_major_endpoints"),
+    re_path(r"^test$", views.test_endpoints, name="test_major_endpoints"),
+    # path("ajax/tests/", views.ajax_query_handler),
+    
 ]
