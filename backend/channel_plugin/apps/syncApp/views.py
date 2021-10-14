ab106e0066cfb378f78572d0350987a09a4ad598
Joseph Adebowale <62403531+Joseph455@users.noreply.github.com>
2021-10-14 01:18:07 +0100

    Implemented QueueHandler class (#3)
    * commit
    
    * Implemented QueueHandler class
    
    * commit

--- backend/channel_plugin/apps/syncApp/views.py
+++ backend/channel_plugin/apps/syncApp/views.py
@@ -1,1 +1,7 @@
+from django.http.response import JsonResponse
 # Create your views here.
+
+
+async def sync_notifier(request):
+    # once the request is recieved call the queue class to start a new queue or add object to the queue
+    return JsonResponse({"status": "ok"}, status=200)
