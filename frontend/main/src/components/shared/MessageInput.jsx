5b63bcd7315f1e6e5b4511d8c38588fd3ee1c6a8
IDOKO EMMANUEL <pidox31102002@gmail.com>
2021-10-15 02:33:32 +0100

    setup message input and message board components

--- frontend/main/src/components/shared/MessageInput.jsx
+++ frontend/main/src/components/shared/MessageInput.jsx
@@ -148,251 +148,255 @@ const MessageInput = ({ channelId}) => {
   formatSelection("b"); // for bold
   formatSelection("strike"); // for strike through
   return (
-    <Box
-      border="1px solid #EBEBEB"
-      borderRadius="4px"
-      marginBottom="10px"
-      onMouseLeave={togglingDisplay}
-      css={{
-        "&::-webkit-scrollbar": {
-          display: "none",
-        },
-      }}
-    >
-      <AddEmojiModal isOpen={isAddEmojiOpen} onClose={onAddEmojiClose} />
-      <Modal
-        isOpen={isMulimediaOpen}
-        onClose={onMulimediaClose}
-        size="xs"
-        isCentered="true"
-        pb={0}
-      >
-        <ModalOverlay />
-        <ModalContent bottom="-5.6rem" left="3.6rem" maxW="20rem">
-          <MultimediaSharingModal />
-        </ModalContent>
-      </Modal>
-      {emoji && (
-        <Container
-          boxShadow="-1px 4px 7px rgba(0, 0, 0, 0.1)"
-          bg="#F9F9F9"
-          textAlign="left"
-          maxWidth="300px"
-          style={{ position: "absolute", top: "10rem", right: "2rem" }}
-        >
-          <Picker
-            pickerStyle={{
-              width: "300px",
-              height: 300,
-              Searchbar: {
-                borderRadius: 20,
-              },
-              fontSize: 10,
-            }}
-            onEmojiClick={onEmojiClick}
-          />
-          <Container p="18px" alignSelf="center">
-            <Text
-              alignItems="center"
-              mt="10px"
-              mb="10px"
-              fontSize="18px"
-              color="#8B8B8B"
-              fontWeight="600"
-            >
-              Frequently Used
-            </Text>
-            <Box>
-              <Button
-                mt="10px"
-                color="#8B8B8B"
-                fontSize="18px"
-                p="7px 11px"
-                border="1px solid #8B8B8B"
-                borderRadius="3px"
-                boxSizing="border-box"
-                bg="white"
-                onClick={onAddEmojiOpen}
-              >
-                Add Emoji
-              </Button>
-            </Box>
-          </Container>
-        </Container>
-      )}
-      <Box display={["none", "block"]}>
-        <ResizableInput
-          textareaRef={textRef}
-          border="none"
-          fontSize="15px"
-          color="neutral.500"
-          placeholder="Message #announcements"
-          _placeholder={{ color: "neutral.500" }}
-          paddingBlock="18px"
-          paddingInline="20px"
-          _focus={{ border: "none" }}
-          value={data}
-          height="50px"
-          changeText={(e) => setData(e.target.value)}
-          onInput={() => setOnInput(true)}
-          fontWeight={active}
-          fontStyle={italic}
-          onMouseDown={formatSelection}
-          onBlur={() => setOnInput(false)}
-        />
-        <Box
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          m={3}
-        >
-          <Box
-            display="flex"
-            _hover={{ cursor: "pointer" }}
-            minW="80px"
-            width="7em"
-            justifyContent="space-between"
-          >
-            <IoFlashOutline />
-            <Divider orientation="vertical" height="20px" color="gray.500" />
-            <BsTypeBold
-              className="box"
-              data-command="bold"
-              onClick={changeWeight}
-            />
-            <FiItalic
-              className="box"
-              data-command="italic"
-              onClick={changeStyle}
-            />
-            <BsLink45Deg />
-            <AiOutlineBars data-command="insertUnorderedList" />
-          </Box>
-          <Box
-            display="flex"
-            _hover={{ cursor: "pointer" }}
-            alignItems="center"
-            minW="80px"
-            width="10em"
-            justifyContent="space-between"
-          >
-            <FiAtSign className="tagged" onClick={addTag} />
-            <ImAttachment onClick={onMulimediaOpen} />
-            <GrEmoji onClick={() => setEmoji(!emoji)} />
-            {input || data !== "" ? (
-              <IoSendSharp color="black" onClick={loadData} />
-            ) : (
-              <Button size="xs" disabled>
-                <IoSendSharp />
-              </Button>
-            )}
-            <Divider orientation="vertical" height="20px" color="gray.500" />
-            <RiArrowDropDownLine size="30px" />
-          </Box>
-        </Box>
-      </Box>
-      <Box
-        display={["flex", "none"]}
-        overflowX="auto"
-        alignItems="center"
-        css={{
-          "&::-webkit-scrollbar": {
-            display: "none",
-          },
-        }}
-      >
-        {click ? (
-          <Box display="flex" flexDir="column" width="100%" mx={2}>
-            <Flex
-              width="100%"
-              dir="row"
-              justify="space-between"
-              alignItems="center"
-              minW="12em"
-            >
-              <ResizableInput
-                textareaRef={textRef}
-                border="none"
-                fontSize="15px"
-                color="neutral.500"
-                placeholder="Message #announcement"
-                _placeholder={{ color: "neutral.500" }}
-                paddingBlock="18px"
-                paddingInline="20px"
-                _focus={{ border: "none" }}
-                value={data}
-                height="58px"
-                changeText={(e) => setData(e.target.value)}
-                onInput={() => setOnInput(true)}
-                fontWeight={active}
-                fontStyle={italic}
-                onBlur={() => setOnInput(false)}
-              />
-              {input || data !== "" ? (
-                <Button bg="#00B87C" size="xs">
-                  <IoSendSharp color="white" onClick={loadData} />
-                </Button>
-              ) : (
-                <Button size="xs" disabled>
-                  <IoSendSharp />
-                </Button>
-              )}
-            </Flex>
-            <Flex
-              justifyContent="space-between"
-              width="12em"
-              m="10px"
-              _hover={{ cursor: "pointer" }}
-            >
-              <IoFlashOutline />
-              <GrEmoji onClick={() => setEmoji(!emoji)} />
-              <BsTypeBold
-                data-command="bold"
-                className="box"
-                onClick={changeWeight}
-              />
-              <FiItalic
-                data-command="italic"
-                className="box"
-                onClick={changeStyle}
-              />
-              <BsLink45Deg />
-              <AiOutlineBars />
-              <FiAtSign onClick={addTag} />
-              <ImAttachment onClick={onMulimediaOpen} />
-            </Flex>
-          </Box>
-        ) : (
-          <Box
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            justifyContent="space-between"
-            width="100%"
-            onClick={() => setOnClick(true)}
-          >
-            <ResizableInput
-              border="none"
-              fontSize="15px"
-              color="neutral.500"
-              placeholder="Message #announcement"
-              _placeholder={{ color: "neutral.500" }}
-              _focus={{ border: "none" }}
-              value={data}
-              paddingBlock="18px"
-              height="58px"
-              changeText={(e) => setData(e.target.value)}
-              onInput={() => setOnInput(true)}
-            />
-            <Flex minW="6em" justifyContent="space-around" alignItems="center">
-              <AiOutlineBars data-command="insertUnorderedList" />
-              <FiAtSign className="tagged" onClick={addTag} />
-              <ImAttachment onClick={onMulimediaOpen} />
-            </Flex>
-          </Box>
-        )}
-      </Box>
-    </Box>
+    <>
+    {/* <CommentBox  sendMessageHandler={console.log} /> */}
+    {/* <MessageBoard /> */}
+    </>
+    // <Box
+    //   border="1px solid #EBEBEB"
+    //   borderRadius="4px"
+    //   marginBottom="10px"
+    //   onMouseLeave={togglingDisplay}
+    //   css={{
+    //     "&::-webkit-scrollbar": {
+    //       display: "none",
+    //     },
+    //   }}
+    // >
+    //   <AddEmojiModal isOpen={isAddEmojiOpen} onClose={onAddEmojiClose} />
+    //   <Modal
+    //     isOpen={isMulimediaOpen}
+    //     onClose={onMulimediaClose}
+    //     size="xs"
+    //     isCentered="true"
+    //     pb={0}
+    //   >
+    //     <ModalOverlay />
+    //     <ModalContent bottom="-5.6rem" left="3.6rem" maxW="20rem">
+    //       <MultimediaSharingModal />
+    //     </ModalContent>
+    //   </Modal>
+    //   {emoji && (
+    //     <Container
+    //       boxShadow="-1px 4px 7px rgba(0, 0, 0, 0.1)"
+    //       bg="#F9F9F9"
+    //       textAlign="left"
+    //       maxWidth="300px"
+    //       style={{ position: "absolute", top: "10rem", right: "2rem" }}
+    //     >
+    //       <Picker
+    //         pickerStyle={{
+    //           width: "300px",
+    //           height: 300,
+    //           Searchbar: {
+    //             borderRadius: 20,
+    //           },
+    //           fontSize: 10,
+    //         }}
+    //         onEmojiClick={onEmojiClick}
+    //       />
+    //       <Container p="18px" alignSelf="center">
+    //         <Text
+    //           alignItems="center"
+    //           mt="10px"
+    //           mb="10px"
+    //           fontSize="18px"
+    //           color="#8B8B8B"
+    //           fontWeight="600"
+    //         >
+    //           Frequently Used
+    //         </Text>
+    //         <Box>
+    //           <Button
+    //             mt="10px"
+    //             color="#8B8B8B"
+    //             fontSize="18px"
+    //             p="7px 11px"
+    //             border="1px solid #8B8B8B"
+    //             borderRadius="3px"
+    //             boxSizing="border-box"
+    //             bg="white"
+    //             onClick={onAddEmojiOpen}
+    //           >
+    //             Add Emoji
+    //           </Button>
+    //         </Box>
+    //       </Container>
+    //     </Container>
+    //   )}
+    //   <Box display={["none", "block"]}>
+    //     <ResizableInput
+    //       textareaRef={textRef}
+    //       border="none"
+    //       fontSize="15px"
+    //       color="neutral.500"
+    //       placeholder="Message #announcements"
+    //       _placeholder={{ color: "neutral.500" }}
+    //       paddingBlock="18px"
+    //       paddingInline="20px"
+    //       _focus={{ border: "none" }}
+    //       value={data}
+    //       height="50px"
+    //       changeText={(e) => setData(e.target.value)}
+    //       onInput={() => setOnInput(true)}
+    //       fontWeight={active}
+    //       fontStyle={italic}
+    //       onMouseDown={formatSelection}
+    //       onBlur={() => setOnInput(false)}
+    //     />
+    //     <Box
+    //       display="flex"
+    //       justifyContent="space-between"
+    //       alignItems="center"
+    //       m={3}
+    //     >
+    //       <Box
+    //         display="flex"
+    //         _hover={{ cursor: "pointer" }}
+    //         minW="80px"
+    //         width="7em"
+    //         justifyContent="space-between"
+    //       >
+    //         <IoFlashOutline />
+    //         <Divider orientation="vertical" height="20px" color="gray.500" />
+    //         <BsTypeBold
+    //           className="box"
+    //           data-command="bold"
+    //           onClick={changeWeight}
+    //         />
+    //         <FiItalic
+    //           className="box"
+    //           data-command="italic"
+    //           onClick={changeStyle}
+    //         />
+    //         <BsLink45Deg />
+    //         <AiOutlineBars data-command="insertUnorderedList" />
+    //       </Box>
+    //       <Box
+    //         display="flex"
+    //         _hover={{ cursor: "pointer" }}
+    //         alignItems="center"
+    //         minW="80px"
+    //         width="10em"
+    //         justifyContent="space-between"
+    //       >
+    //         <FiAtSign className="tagged" onClick={addTag} />
+    //         <ImAttachment onClick={onMulimediaOpen} />
+    //         <GrEmoji onClick={() => setEmoji(!emoji)} />
+    //         {input || data !== "" ? (
+    //           <IoSendSharp color="black" onClick={loadData} />
+    //         ) : (
+    //           <Button size="xs" disabled>
+    //             <IoSendSharp />
+    //           </Button>
+    //         )}
+    //         <Divider orientation="vertical" height="20px" color="gray.500" />
+    //         <RiArrowDropDownLine size="30px" />
+    //       </Box>
+    //     </Box>
+    //   </Box>
+    //   <Box
+    //     display={["flex", "none"]}
+    //     overflowX="auto"
+    //     alignItems="center"
+    //     css={{
+    //       "&::-webkit-scrollbar": {
+    //         display: "none",
+    //       },
+    //     }}
+    //   >
+    //     {click ? (
+    //       <Box display="flex" flexDir="column" width="100%" mx={2}>
+    //         <Flex
+    //           width="100%"
+    //           dir="row"
+    //           justify="space-between"
+    //           alignItems="center"
+    //           minW="12em"
+    //         >
+    //           <ResizableInput
+    //             textareaRef={textRef}
+    //             border="none"
+    //             fontSize="15px"
+    //             color="neutral.500"
+    //             placeholder="Message #announcement"
+    //             _placeholder={{ color: "neutral.500" }}
+    //             paddingBlock="18px"
+    //             paddingInline="20px"
+    //             _focus={{ border: "none" }}
+    //             value={data}
+    //             height="58px"
+    //             changeText={(e) => setData(e.target.value)}
+    //             onInput={() => setOnInput(true)}
+    //             fontWeight={active}
+    //             fontStyle={italic}
+    //             onBlur={() => setOnInput(false)}
+    //           />
+    //           {input || data !== "" ? (
+    //             <Button bg="#00B87C" size="xs">
+    //               <IoSendSharp color="white" onClick={loadData} />
+    //             </Button>
+    //           ) : (
+    //             <Button size="xs" disabled>
+    //               <IoSendSharp />
+    //             </Button>
+    //           )}
+    //         </Flex>
+    //         <Flex
+    //           justifyContent="space-between"
+    //           width="12em"
+    //           m="10px"
+    //           _hover={{ cursor: "pointer" }}
+    //         >
+    //           <IoFlashOutline />
+    //           <GrEmoji onClick={() => setEmoji(!emoji)} />
+    //           <BsTypeBold
+    //             data-command="bold"
+    //             className="box"
+    //             onClick={changeWeight}
+    //           />
+    //           <FiItalic
+    //             data-command="italic"
+    //             className="box"
+    //             onClick={changeStyle}
+    //           />
+    //           <BsLink45Deg />
+    //           <AiOutlineBars />
+    //           <FiAtSign onClick={addTag} />
+    //           <ImAttachment onClick={onMulimediaOpen} />
+    //         </Flex>
+    //       </Box>
+    //     ) : (
+    //       <Box
+    //         display="flex"
+    //         flexDirection="row"
+    //         alignItems="center"
+    //         justifyContent="space-between"
+    //         width="100%"
+    //         onClick={() => setOnClick(true)}
+    //       >
+    //         <ResizableInput
+    //           border="none"
+    //           fontSize="15px"
+    //           color="neutral.500"
+    //           placeholder="Message #announcement"
+    //           _placeholder={{ color: "neutral.500" }}
+    //           _focus={{ border: "none" }}
+    //           value={data}
+    //           paddingBlock="18px"
+    //           height="58px"
+    //           changeText={(e) => setData(e.target.value)}
+    //           onInput={() => setOnInput(true)}
+    //         />
+    //         <Flex minW="6em" justifyContent="space-around" alignItems="center">
+    //           <AiOutlineBars data-command="insertUnorderedList" />
+    //           <FiAtSign className="tagged" onClick={addTag} />
+    //           <ImAttachment onClick={onMulimediaOpen} />
+    //         </Flex>
+    //       </Box>
+    //     )}
+    //   </Box>
+    // </Box>
   );
 };
 
