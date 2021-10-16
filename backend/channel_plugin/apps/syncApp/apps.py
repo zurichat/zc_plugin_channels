e23f5cc33df01108714e3d541409fa7590e06a9c
Joseph Adebowale <adebowalejojo2001@gmail.com>
2021-10-16 20:36:27 +0100

    commit

--- backend/channel_plugin/apps/syncApp/apps.py
+++ backend/channel_plugin/apps/syncApp/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+import threading
+from apps.syncApp.jobs import run_qhandler_schedule
+
+
 class SyncAppConfig(AppConfig):
     name = 'apps.syncApp'
 
     def ready(self):
-        pass
+        try:
+            th = threading.Thread(target=run_qhandler_schedule)
+            th.start()
+        except:
+            pass
+        
