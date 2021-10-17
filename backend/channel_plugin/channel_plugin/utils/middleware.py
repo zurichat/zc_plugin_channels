a01259574ea73c15f1e2702af15d3373cba18cd9
Charles Oraegbu <charlesboy49@gmail.com>
2021-10-17 14:08:05 +0100

    sidebar patch

--- backend/channel_plugin/channel_plugin/utils/middleware.py
+++ backend/channel_plugin/channel_plugin/utils/middleware.py
@@ -38,26 +38,22 @@ class CorsMiddleware:
             response = self.process_response(request, response)
             capture_message(f'Production - {response.__dict__["_headers"]}')
 
-            try:
-                del response.__dict__["_headers"]["access-control-allow-origin"]
-            except KeyError:
-                pass
-
         return response
 
     def process_response(self, request, response):
 
-        # result = host_regex.match(request.get_host())
+        result = host_regex.match(request.get_host())
         if request.method in ["GET"]:
             try:
                 del response.__dict__["_headers"]["access-control-allow-origin"]
             except KeyError:
                 pass
 
-        # if request.method in ["POST", "PUT", "DELETE"] and result:
-        #     response.__dict__["_headers"]["access-control-allow-origin"] = (
-        #         "Access-Control-Allow-Origin",
-        #         request.get_host(),
-        #     )
+        if request.method in ["POST", "PUT", "DELETE"] and result:
+
+            try:
+                del response.__dict__["_headers"]["access-control-allow-origin"]
+            except KeyError:
+                pass
 
         return response
